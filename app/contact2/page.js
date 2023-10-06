05358fa1a02da3990f1e2a1ad23956b6bb328b9a
Derek <derek.yadlowski@sait.ca>
2023-10-06 13:51:21 -0600

    Updated for week 5

---
+++ app/contact2/page.js
@@ -0,0 +1,9 @@
+import ContactList from "./contact-list";
+
+export default function Page(){
+    return (
+        <main className="h-screen bg-sky-100">
+            <ContactList />
+        </main>
+    );
+}
